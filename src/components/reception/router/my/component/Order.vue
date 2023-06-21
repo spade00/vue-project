e632058e241e840e8851ed71d1d527170d68bf08
Ren <212835313@qq.com>
2023-06-21 10:00:34 +0800

    变更

--- src/components/reception/router/my/component/Order.vue
+++ src/components/reception/router/my/component/Order.vue
@@ -21,7 +21,10 @@
     margin: 20px;
 }
 .Order_bg{
-
+    display: flex;
+    height: 80vh;
+    flex-direction: column;
+    justify-content: space-between;
 }
 .order_paging{
     display: flex;
@@ -30,39 +33,41 @@
 </style>
 <template>
     <div class="Order_bg">
-        <div class="Order" v-for="(item,index) in paginatedData">
-            <Card style="width:100%">
-                <template #title>
-                    <Icon type="ios-film-outline"></Icon>
-                    {{item.payment_time}}
-                    <span>&nbsp;&nbsp;&nbsp;&nbsp;订单号{{item.payment_id}}</span>
-                </template>
-                <template #extra>
-                    <a href="#" @click.prevent="remove_order(index)">
-                        <Icon type="md-trash" />
-                    </a>
-                </template>
-                <div class="order-content">
-                    <div class="details">
-                        <img src="src/assets/movie/fb73869a3390fa281e395ba5f89fe7142e4ab.jpg" alt="海报" width="100">
-                        <div class="text">
-                            <h3>{{item.title}}</h3>
-                            <p>{{item.cinema}}</p>
-                            <p>{{ item.screens }}    {{item.row}}排{{item.col}}座</p>
-                            <h3 style="color: red">{{ item.date }}</h3>
+        <div>
+            <div class="Order" v-for="(item,index) in paginatedData">
+                <Card style="width:100%">
+                    <template #title>
+                        <Icon type="ios-film-outline"></Icon>
+                        {{}}
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;订单号{{item.PaymentId}}</span>
+                    </template>
+                    <template #extra>
+                        <a href="#" @click.prevent="remove_order(index)">
+                            <Icon type="md-trash" />
+                        </a>
+                    </template>
+                    <div class="order-content">
+                        <div class="details">
+                            <img src="src/assets/movie/fb73869a3390fa281e395ba5f89fe7142e4ab.jpg" alt="海报" width="100">
+                            <div class="text">
+                                <h3>{{item.Title}}</h3>
+                                <p>鞍山影院</p>
+                                <p>{{ item.Halls }}    票X{{item.Number}}</p>
+                                <h3 style="color: red">{{ item.Data }}</h3>
+                            </div>
+                        </div>
+                        <h3 style="color: red">
+                            ￥{{item.Prices}}
+                        </h3>
+                        <div>
+                            出票成功
+                        </div>
+                        <div>
+                            <Button @click="order_details(item.payment_id)">查看详情</Button>
                         </div>
                     </div>
-                    <h3 style="color: red">
-                        ￥{{item.prince}}
-                    </h3>
-                    <div>
-                        {{ item.state }}
-                    </div>
-                    <div>
-                        <Button @click="order_details(item.payment_id)">查看详情</Button>
-                    </div>
-                </div>
-            </Card>
+                </Card>
+            </div>
         </div>
         <div class="order_paging">
             <n-pagination v-model:page="order_info.currentPage" :page-count="pageCount" size="large"/>
@@ -71,8 +76,9 @@
 </template>
 <script>
 import {Button, Card, Icon, Page, Rate} from "view-ui-plus";
-import {computed, reactive} from "vue";
+import {computed, onMounted, reactive} from "vue";
 import router from "@/router";
+import request from "@/utils/request";
 
 export default {
     components: {Page, Button, Rate, Icon, Card},
@@ -80,77 +86,15 @@ export default {
         const order_info = reactive({
             data:[
                 {
-                    payment_id:"123456789",
-                    payment_time:"2023-05-01 22:37:20",
-                    title:"天空之城",
-                    cinema:"鞍山影院",
-                    screens:"5号激光厅",
-                    row:"1",
-                    col:"1",
-                    prince:24,
-                    date:"2023年10月01日",
+                    PaymentId:"123456789",
+                    PaymentTime:"2023-05-01 22:37:20",
+                    Title:"天空之城",
+                    Halls:"5号激光厅",
+                    Number:"4",
+                    Prices:120,
+                    Data:"2023年10月01日",
                     state:"出票成功"
-                },
-                {
-                    payment_id:"123456",
-                    payment_time:"2023-05-01 22:37:20",
-                    title:"天空之城",
-                    cinema:"鞍山影院",
-                    screens:"5号激光厅",
-                    row:"1",
-                    col:"1",
-                    prince:24,
-                    date:"2023年10月01日",
-                    state:"出票成功"
-                },
-                {
-                    payment_id:"1234567",
-                    payment_time:"2023-05-01 22:37:20",
-                    title:"天空之城",
-                    cinema:"鞍山影院",
-                    screens:"5号激光厅",
-                    row:"1",
-                    col:"1",
-                    prince:24,
-                    date:"2023年10月01日",
-                    state:"出票成功"
-                },
-                {
-                    payment_id:"12345678",
-                    payment_time:"2023-05-01 22:37:20",
-                    title:"天空之城",
-                    cinema:"鞍山影院",
-                    screens:"5号激光厅",
-                    row:"1",
-                    col:"1",
-                    prince:24,
-                    date:"2023年10月01日",
-                    state:"出票成功"
-                },
-                {
-                    payment_id:"00005678900",
-                    payment_time:"2023-05-01 22:37:20",
-                    title:"天空之城",
-                    cinema:"鞍山影院",
-                    screens:"5号激光厅",
-                    row:"1",
-                    col:"2",
-                    prince:30,
-                    date:"2023年10月01日",
-                    state:"出票成功"
-                },
-                {
-                    payment_id:"1234560000",
-                    payment_time:"2023-05-01 22:37:20",
-                    title:"天空之城",
-                    cinema:"鞍山影院",
-                    screens:"5号激光厅",
-                    row:"1",
-                    col:"3",
-                    prince:50,
-                    date:"2023年10月01日",
-                    state:"出票成功"
-                },
+                }
             ],
             currentPage:1,//当前页
             pageSize:3,//每页显示数据条数
@@ -175,6 +119,23 @@ export default {
                 }
             })
         }
+        onMounted(()=>{
+            if (localStorage.getItem("Login_state")===null){
+                router.push("/login")
+            }
+            request.get("/Item/my",{
+                id:localStorage.getItem("Login_state")
+            }).then(res=>{
+                order_info.data.push(...JSON.parse(res.data))
+                console.log(order_info.data)
+            }).catch(error=>{
+                console.log(error)
+                alert("登录状态异常，重新登录")
+                localStorage.removeItem("jwt")
+                localStorage.removeItem("Login_state")
+                router.push("/Login")
+            })
+        })
         return{
             order_info,
             paginatedData,
